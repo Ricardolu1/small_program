9dc946be7b3f12be85321863c18b7636820ea72e
Ricardo <568082704@qq.com>
2019-11-30 20:01:33 +0800

    save

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,66 +1,26 @@
 // pages/posts/posts.js
-Page({
+var postData = require('../../data/post-data.js')
 
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    date:"Nov 11 2020",
+    title:"正是虾肥蟹壮时"
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function () {
+   
+    this.setData({ 
+      postList:postData.postList
+     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onPostTap:function(e){
+    var postId = e.currentTarget.dataset.postId
+    wx.navigateTo({
+      url: './post-detail/post-detail?id='+postId,
+    })
   }
+ 
 })
